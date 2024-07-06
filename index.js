39309f2fc10a241129d0e6e65f19c99c667da52f
Harikrishh123 <nvsatyamnwelamsetti@gmail.com>
2024-07-06 12:45:18 +0530

    Update

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ connectToMongo();
 const app = express()
 const port = 3001
 var cors = require('cors')
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 app.use(express.json())
 
 app.use('/auth', require("./routes/auth.js"))
